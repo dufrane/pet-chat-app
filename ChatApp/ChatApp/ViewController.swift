4d37e91ced45e48bd7787c39919478845eff47f2
dufrane <dmitriy.vasilenko@gmail.com>
2022-11-03 06:51:02 +0100

    Initial commit

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Dmytro Vasylenko on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
